38179fd92ad2afcc5cd0ee4688486b2b98065b51
SaltineChips <rubixblockchain@gmail.com>
2019-09-08 16:53:57 -0700

    Revert "Merge pull request #9 from CryptoCoderz/master"
    This reverts commit 8f14943a454cf3a5f0f00eff0b8097a281ac9913, reversing
    changes made to f9c840e3c837df922337f25c83e396e7336efffb.

--- src/net.cpp
+++ src/net.cpp
@@ -615,14 +615,16 @@ void SocketSendData(CNode *pnode)
                 break;
             }
         } else {
-            if (nBytes == 0) {
-                // couldn't send anything at all
-                LogPrintf("socket send error: data failure\n");
-                pnode->CloseSocketDisconnect();
-                break;
+            if (nBytes < 0) {
+                // error
+                int nErr = WSAGetLastError();
+                if (nErr != WSAEWOULDBLOCK && nErr != WSAEMSGSIZE && nErr != WSAEINTR && nErr != WSAEINPROGRESS)
+                {
+                    LogPrintf("socket send error %d\n", nErr);
+                    pnode->CloseSocketDisconnect();
+                }
             }
-
-            pnode->CloseSocketDisconnect();
+            // couldn't send anything at all
             break;
         }
     }
